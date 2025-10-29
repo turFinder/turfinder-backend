e9556e430dfbd3102a966400a74d65cac62c276c
Masruwur <mas.morshed2424@gmail.com>
2025-10-29 12:11:32 +0600

    security update

--- turfinder-backend/src/main/java/net/tufinder/backend/config/SecurityConfig.java
+++ turfinder-backend/src/main/java/net/tufinder/backend/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
-        httpSecurity.cors(cors->{});
+        httpSecurity.cors(Customizer.withDefaults());
         httpSecurity.authorizeHttpRequests(request->
                                            request.requestMatchers("users/login","users/create","users/google/login")
                                                    .permitAll()
